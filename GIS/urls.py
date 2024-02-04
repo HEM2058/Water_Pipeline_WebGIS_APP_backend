46aacd79ea81d570823d9f56936389ff9f442a76
HEM2058 <hemrajpandeyd@gmail.com>
2024-02-05 04:21:33 +0545

    added new view for the geojson conversion

--- GIS/urls.py
+++ GIS/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import GeoDataListAPIView
+from .views import GeoDataListAPIView, GeoJSONAPIView
 
 urlpatterns = [
-    path('api/geo-data/', GeoDataListAPIView.as_view(), name='geo-data-list'),
+    path('geo-data/', GeoDataListAPIView.as_view(), name='geo-data-list'),
+      path('geo-json/', GeoJSONAPIView.as_view(), name='geo-json'),
     # Add other URLs as needed
 ]
--- GIS/views.py
+++ GIS/views.py
@@ -2,7 +2,41 @@
 from rest_framework import generics
 from .models import GeoData
 from .serializers import GeoDataSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.gis.geos import GEOSGeometry
+from django.core.serializers import serialize
+import json
 
 class GeoDataListAPIView(generics.ListAPIView):
     queryset = GeoData.objects.all()
     serializer_class = GeoDataSerializer
+
+
+class GeoJSONAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        # Query GeoData objects
+        geo_data = GeoData.objects.all()
+
+        # Convert GeoData queryset to GeoJSON format
+        features = []
+        for data in geo_data:
+            geometry = json.loads(data.geometry.geojson)
+            
+            feature = {
+                "type": "Feature",
+                "geometry": geometry,
+                "properties": {
+                    "id": data.id,
+                    "name": data.name,
+                    "diameter": data.diameter,
+                }
+            }
+            features.append(feature)
+
+        feature_collection = {
+            "type": "FeatureCollection",
+            "features": features,
+        }
+
+        return Response(feature_collection)zzzzzzzzzzzzzzzz
